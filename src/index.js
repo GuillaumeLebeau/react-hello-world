17d16a9aacd32978c19f568f10a6cd319606685c
Guillaume Lebeau <glebeau.dev@gmail.com>
2017-09-17 21:08:25 +0200

    JSX is an Expression Too
    https://facebook.github.io/react/docs/introducing-jsx.html#jsx-is-an-expression-too

--- src/index.js
+++ src/index.js
@@ -8,15 +8,21 @@ function formatName(user) {
     return user.firstName + ' ' + user.lastName;
 }
 
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello, {formatName(user)}!</h1>
+    }
+
+    return <h1>Hello, Stranger.</h1>
+}
+
 const user = {
     firstName: 'Guillaume',
     lastName: 'Lebeau'
 };
 
 const element = (
-    <h1>
-        Hello, {formatName(user)}!
-    </h1>
+    getGreeting(user)
 );
 
 ReactDOM.render(
