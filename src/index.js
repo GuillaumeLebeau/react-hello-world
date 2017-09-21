8f6d2f79a53d6cfb1d7c98f960b93b5862a76fca
Guillaume Lebeau <glebeau.dev@gmail.com>
2017-09-21 22:19:39 +0200

    Rendering a Component
    https://facebook.github.io/react/docs/components-and-props.html#rendering-a-component

--- src/index.js
+++ src/index.js
@@ -1,53 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
 
-function formatName(user) {
-    return user.firstName + ' ' + user.lastName;
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
 }
 
-function getGreeting(user) {
-    if (user) {
-        return <h1>Hello, {formatName(user)}!</h1>
-    }
-
-    return <h1>Hello, Stranger.</h1>
-}
-
-const user = {
-    firstName: 'Guillaume',
-    lastName: 'Lebeau',
-    avatarUrl: 'https://avatars2.githubusercontent.com/u/2910740?v=4&u=2ffd5540cdb3d888518eebc171f55177a66db921&s=400'
-};
-
-const userAvatarElement = <img src={user.avatarUrl} alt="avatar"></img>;
-
-const element = (
-    <section>
-        <div>
-            {getGreeting(user)}
-        </div>
-        <div>
-            {userAvatarElement}
-        </div>
-    </section>
+const element = <Welcome name="Sara" />;
+ReactDOM.render(
+  element,
+  document.getElementById('root')
 );
-
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
-  );
-}
-
-setInterval(tick, 1000);
-
-// registerServiceWorker();
