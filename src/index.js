45eb9e31a5f53a6da7cc37c9b8f0cb09e7f304ae
Guillaume Lebeau <glebeau.dev@gmail.com>
2017-09-21 22:08:08 +0200

    Updating the Rendered Element
    https://facebook.github.io/react/docs/rendering-elements.html#updating-the-rendered-element

--- src/index.js
+++ src/index.js
@@ -35,8 +35,19 @@ const element = (
     </section>
 );
 
-ReactDOM.render(
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  ReactDOM.render(
     element,
     document.getElementById('root')
-);
+  );
+}
+
+setInterval(tick, 1000);
+
 // registerServiceWorker();
